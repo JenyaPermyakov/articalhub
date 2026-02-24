b078acf6e098da1d8effb9d5eb8cd10cdcbfcba2
Yevgeniy <jekap99@mail.ru>
2026-02-24 18:56:53 +0500

    update

--- articalehub/articles/models.py
+++ articalehub/articles/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Article(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField(default=False)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='articles', to=articles)
+
+    def __str__(self):
+        return self.title
